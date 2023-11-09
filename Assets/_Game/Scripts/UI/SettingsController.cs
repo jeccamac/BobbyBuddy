0c2241ba4912232074a9f43bea2fd784ed9b625c
Jessica Macaspac <jeccamac@gmail.com>
2023-11-09 09:26:39 -0600

    Audio settings toggle image
    Set toggle images for music and sound in settings

--- Assets/_Game/Scripts/UI/SettingsController.cs
+++ Assets/_Game/Scripts/UI/SettingsController.cs
@@ -5,22 +5,62 @@ using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
-    [SerializeField] public Slider _musicSlider, _sfxSlider;
+    [Header("Sliders")]
+    [SerializeField] public Slider _musicSlider;
+    [SerializeField] public Slider _sfxSlider;
+
+    [Header("Buttons")]
+    [SerializeField] private Image musicButton;
+    [SerializeField] private Image sfxButton;
+
+    [Header("Sprite Settings")]
+    [SerializeField] private Sprite musicOn;
+    [SerializeField] private Sprite musicOff;
+    [SerializeField] private Sprite soundOn;
+    [SerializeField] private Sprite  soundOff;
+
+    private int musicCount = 0;
+    private int soundCount = 0;
 
     private void Start() 
     {
         _musicSlider.value = AudioManager.Instance.musicSource.volume;
         _sfxSlider.value = AudioManager.Instance.sfxSource.volume;
+        musicButton = GameObject.Find("Music_Button").GetComponent<Image>();
+        sfxButton = GameObject.Find("Sound_Button").GetComponent<Image>();
+
+        musicButton.sprite = musicOn;
+        sfxButton.sprite = soundOn;
     }
 
     public void ToggleMusic()
     {
         AudioManager.Instance.ToggleMusic();
+
+        if (musicCount == 0)
+        {
+            musicButton.sprite = musicOff;
+            musicCount++;
+        } else
+        { 
+            musicButton.sprite = musicOn;
+            musicCount = 0;
+        }
     }
 
     public void ToggleSFX()
     {
         AudioManager.Instance.ToggleSFX();
+
+        if (soundCount == 0)
+        {
+            sfxButton.sprite = soundOff;
+            soundCount++;
+        } else
+        {
+            sfxButton.sprite = soundOn;
+            soundCount = 0;
+        }
     }
 
     public void MusicVolume()
