f9e71106081b80917eac1bc4243b72ffed70587b
Jessica Macaspac <jeccamac@gmail.com>
2023-11-13 12:51:58 -0600

    Tooth health update color edit
    Tooth health color updates from scene to scene

--- Assets/_Game/Scripts/UI/ToothHealth.cs
+++ Assets/_Game/Scripts/UI/ToothHealth.cs
@@ -17,7 +17,7 @@ public class ToothHealth : MonoBehaviour
     [Header("Health Color Status")]
     [SerializeField] private Color healthy;
     [SerializeField] private Color yellow;
-    [SerializeField] private Color rot;
+    [SerializeField] private Color critical;
 
     [Header("Tooth Icon Images")]
     [SerializeField] private Sprite healthy1, healthy2, yellow1, yellow2, rot1, rot2;
@@ -27,54 +27,82 @@ public class ToothHealth : MonoBehaviour
     private void Start() 
     {
         textDisplay = FindObjectOfType<TextDisplay>();
+
         _dentalState = DataManager.Instance.dentalState;
         _currentHealth = DataManager.Instance.currentHealth;
         _maxHealth = DataManager.Instance.maxHealth;
+
         SwapIcons();
     }
 
     private void Update()
     {
         //get health info from data manager
-        _dentalState = DataManager.Instance.dentalState;
-        _currentHealth = DataManager.Instance.currentHealth;
-        _maxHealth = DataManager.Instance.maxHealth;
-        healthBar.fillAmount = _currentHealth / _maxHealth;
-
+        GetHealth();
+        
         //update tooth health display
         SwapIcons();
     }
 
+    public void GetHealth()
+    {
+        _dentalState = DataManager.Instance.dentalState;
+        _currentHealth = DataManager.Instance.currentHealth;
+        _maxHealth = DataManager.Instance.maxHealth;
+        healthBar.fillAmount = _currentHealth / _maxHealth;
+    }
+
     public void SwapIcons()
     {
+            //icons
         if (_dentalState == 5)
         {
-            toothIcon.sprite = healthy1;
+            toothIcon.sprite = healthy1;  
             healthBar.color = healthy;
         }
         else if (_dentalState == 4)
         {
             toothIcon.sprite = healthy2;
+            healthBar.color = healthy;
         }
         else if (_dentalState == 3)
         {
             toothIcon.sprite = yellow1;
             healthBar.color = yellow;
-                //SHOW TEXT SOMEWHERE, IT'S CONSTANT IN THIS FUNCTION, NEEDS TO RUN ONCE
-            //textDisplay.ShowText("I should take better care of my teeth", 3f);
         }
         else if (_dentalState == 2)
         {
             toothIcon.sprite = yellow2;
+            healthBar.color = yellow;
         }
         else if (_dentalState == 1)
         {
             toothIcon.sprite = rot1;
-            healthBar.color = rot;
-            //textDisplay.ShowText("I might want visit the dentist", 3f);
-        } else if (_dentalState == 0)
+            healthBar.color = critical;
+        } 
+        else if (_dentalState == 0)
         {
-            //textDisplay.ShowText("I should really get my teeth checked by the dentist", 3f);
+            toothIcon.sprite = rot2;
+            healthBar.color = critical;
         }
+
+        //     //bar color
+        // if (_currentHealth >= 60)
+        // {
+        //     healthBar.color = healthy;
+        // } 
+        // else if (_currentHealth <= 60 && _currentHealth >= 20)
+        // {
+        //     healthBar.color = yellow;
+        // } 
+        // else if (_currentHealth <= 20)
+        // {
+        //     healthBar.color = critical;
+        // }
     }
+
+        //SHOW TEXT SOMEWHERE, IT'S CONSTANT IN THIS FUNCTION, NEEDS TO RUN ONCE
+    //textDisplay.ShowText("I should take better care of my teeth", 3f);
+    //textDisplay.ShowText("I might want visit the dentist", 3f);
+    //textDisplay.ShowText("I should really get my teeth checked by the dentist", 3f);
 }
