97f381a0a0acb25c9ebe3fee7202baa96f47635a
Jessica Macaspac <jeccamac@gmail.com>
2024-03-04 12:09:10 -0600

    Import final Bobby fbx mesh and blender animations

--- Assets/_Game/Scripts/GameManager/EyeLookAtManager.cs
+++ Assets/_Game/Scripts/GameManager/EyeLookAtManager.cs
@@ -8,10 +8,13 @@ public class EyeLookAtManager : MonoBehaviour
     public GameObject objEyeR;
 
     private Renderer renderEyeL, renderEyeR;
-    public Transform objPivotEyeL;
-    public Transform objPivotEyeR;
+    // public Transform objPivotEyeL;
+    // public Transform objPivotEyeR;
     public Transform objLookAtL;
     public Transform objLookAtR;
+
+    [SerializeField] float offsetSpeedX;
+    [SerializeField] float offsetSpeedY;
 
     private void Start() 
     {
@@ -20,37 +23,15 @@ public class EyeLookAtManager : MonoBehaviour
     }
     private void Update() 
     {
-        // objPivotEyeL.LookAt(objLookAtL);
-        // objPivotEyeR.LookAt(objLookAtR);
-
-        //Debug.Log("PIVOT " + objPivotEyeL.position.x + " LOOK AT " + objLookAtL.position.x);
-
-        // //get local eye rotation
-        // var eyeLPivotX = objPivotEyeL.localRotation.x;
-        // var eyeLPivotY = objPivotEyeL.localRotation.y * 3;
-        // Vector2 tempEyeLRot = new Vector2(eyeLPivotY, eyeLPivotX);
-
-        // var eyeRPivotX = objPivotEyeR.localRotation.x;
-        // var eyeRPivotY = objPivotEyeR.localRotation.y * -2;
-        // Vector2 tempEyeRRot = new Vector2(eyeRPivotY, eyeRPivotX);
-
-        //update texture offset
-        // renderEyeL.material.SetTextureOffset("_MainTex", tempEyeRot);
-        // renderEyeR.material.SetTextureOffset("_MainTex", tempEyeRot);
-        // renderEyeL.material.mainTextureOffset = tempEyeLRot;
-        // renderEyeR.material.mainTextureOffset = tempEyeRRot;
-
-        var eyeLPivotX = objLookAtL.position.x / 4;
-        var eyeLPivotY = objLookAtL.position.y / -2;
+        var eyeLPivotX = objLookAtL.position.x / offsetSpeedX;
+        var eyeLPivotY = objLookAtL.position.y / -offsetSpeedY;
         Vector2 eyeLLook = new Vector2(eyeLPivotX, eyeLPivotY);
 
-        var eyeRPivotX = objLookAtR.position.x / -4;
-        var eyeRPivotY = objLookAtR.position.y / -2;
+        var eyeRPivotX = objLookAtR.position.x / -offsetSpeedX;
+        var eyeRPivotY = objLookAtR.position.y / -offsetSpeedY;
         Vector2 eyeRLook = new Vector2(eyeRPivotX, eyeRPivotY);
 
         renderEyeL.material.mainTextureOffset = eyeLLook;
         renderEyeR.material.mainTextureOffset = eyeRLook;
     }
-
-
 }
