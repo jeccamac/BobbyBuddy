a7d477dbe18d14d8d2600b2d952c19ec26d61d26
Jessica Macaspac <jeccamac@gmail.com>
2024-04-16 09:08:04 -0500

    Garden buttons functioning

--- Assets/_Game/Scripts/LevelMechanics/GardenButtons.cs
+++ Assets/_Game/Scripts/LevelMechanics/GardenButtons.cs
@@ -1,16 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GardenButtons : MonoBehaviour
 {
-    [Header("Garden Settings")]
+    [Header("Garden Actions Settings")]
     [SerializeField] public GameObject[] garden = {}; //green, harvest, nightshade
 
     [Header("Toggle UI Settings")]
+    [SerializeField] public PatioActions patioActions = null;
+    [SerializeField] public Animator gardenUI = null;
+    [SerializeField] public GameObject togglePanel = null;
     [SerializeField] public GameObject[] uiToToggle = {};
+    private bool isPanelActive;
 
-    // Update is called once per frame
+    private void Start() 
+    {
+        gardenUI.enabled = false;
+        isPanelActive = false;
+    }
+
+    private void Update()
+    {
+        if (patioActions.moveLeft == 1) 
+        {
+            gardenUI.enabled = true;
+            togglePanel.SetActive(true);
+        } 
+        else 
+        { 
+            isPanelActive = false;
+            togglePanel.SetActive(false); 
+        }
+    }
+    public void TogglePanel()
+    {
+        if (!isPanelActive)
+        { 
+            gardenUI.enabled = true;
+            gardenUI.Play("SlideIn");
+            isPanelActive = true;
+            Debug.Log("panel is active ");
+        } else 
+        { 
+            gardenUI.Play("SlideOut"); 
+            isPanelActive = false;
+            Debug.Log("panel is not active");
+        }
+    }
     public void GardenGreen()
     {
         garden[0].SetActive(true);
